314ecd6247fb4cc21b74a30f516d79430e35cab1
PavlovAlex <al3ksei.pav@yandex.ru>
2023-12-22 11:30:20 +0300

    12 спринт коммит 3

--- app/src/main/java/com/practicum/playlistmaker/util/AppPreferencesKeys.kt
+++ app/src/main/java/com/practicum/playlistmaker/util/AppPreferencesKeys.kt
@@ -1,16 +1,16 @@
 package com.practicum.playlistmaker.util
 
-internal class AppPreferencesKeys { // глобальные переменные\константы
+internal object AppPreferencesKeys { // глобальные объекты
 
-    companion object {
-        // ключи и файлы в хранилище
-        const val PREFS_NAME = "MyPrefs"
-        const val PREFS_HISTORY_NAME = "SearchHistory"
-        const val KEY_NIGHT_MODE = "nightMode"
-        const val KEY_HISTORY_LIST = "key_for_history_list"
-        // числовые константы
-        const val ALBUM_ROUNDED_CORNERS = 8
-        const val SERVER_PROCESSING_MILLISECONDS : Long = 1500
-        const val HISTORY_TRACK_LIST_SIZE = 10
-    }
+    // ключи и файлы в хранилище
+    const val PREFS_NAME = "MyPrefs"
+    const val PREFS_HISTORY_NAME = "SearchHistory"
+    const val KEY_NIGHT_MODE = "nightMode"
+    const val KEY_HISTORY_LIST = "key_for_history_list"
+
+    // числовые константы
+    const val ALBUM_ROUNDED_CORNERS = 8
+    const val SERVER_PROCESSING_MILLISECONDS: Long = 1500
+    const val HISTORY_TRACK_LIST_SIZE = 10
 }
+
