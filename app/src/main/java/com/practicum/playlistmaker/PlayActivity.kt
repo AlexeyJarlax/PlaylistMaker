2fa901b89ba9bf278d1992fdd7e4add61989569b
PavlovAlex <al3ksei.pav@yandex.ru>
2024-01-05 21:36:26 +0300

    начало 13 спринта

---
+++ app/src/main/java/com/practicum/playlistmaker/PlayActivity.kt
@@ -0,0 +1,11 @@
+package com.practicum.playlistmaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play)
+    }
+}
